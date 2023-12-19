ecbc58b9f24544a322da1a499777c7967fe53751
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-12-19 09:49:47 +0300

    task2 solution1

--- task2/task2-functions.cpp
+++ task2/task2-functions.cpp
@@ -1,8 +1,6 @@
 #include "task2-functions.h"
-
-/////////////////////////////////////// DO NOT MODIFY THE FOLLOWING ///////////////////////////////////////
-
-//This is a tested function to calculate and return the area of a polygon with corner coordinates in the array points.
+#include <cmath>
+// This is a tested function to calculate and return the area of a polygon with corner coordinates in the array points.
 // You may call this from within this file
 // You cannot call this directly from other files
 // Do NOT change this function
@@ -16,65 +14,70 @@ static double areaOfPolygon_v1(Point points[], int n) {
     return abs(area / 2.0);
 }
 
-//This is a tested function to calculates and return the length of a line between points p1 and p2
+// This is a tested function to calculate and return the length of a line between points p1 and p2
 // You may call this from within this file
 // You cannot call this directly from other files
 // Do NOT change this function
-static double distanceBetweenPoints(Point p1, Point p2)
-{
+static double distanceBetweenPoints(Point p1, Point p2) {
     double xd = p1.x - p2.x;
     double yd = p1.y - p2.y;
     double d = sqrt(xd * xd + yd * yd); // Good ol' Pythogoras
     return d;
 }
 
-//Display a point in the terminal
+// Display a point in the terminal
 // Do NOT change this function
-void displayPoint(Point p)
-{
+void displayPoint(Point p) {
     cout.precision(1);
     cout << std::fixed;
     cout << "(" << p.x << "," << p.y << ")" << endl;
 }
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////
- ////////////////////////////////////// YOUR SOLUTION STARTS HERE ///////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//You need to complete this
-//
-// This accepts a vector of points (read from the file)
-// You need to call the areaOfPolygon_v1 function. However, that accepts an array or points, not a vector
 double areaOfPolygon(vector<Point> points) {
+    // Convert vector<Point> to an array of Point
+    Point *pointArray = &points[0];
 
-    // WRITE SOLUTION HERE
-
-    //Change this
-    return 0.0;
+    // Call the provided areaOfPolygon_v1 function
+    return areaOfPolygon_v1(pointArray, points.size());
 }
 
-//You need to complete this
-//
-// The parameter is a vector of points for a shape.
-// Calculate the circumference of this shape.
-// Make use of the distanceBetweenPoints function to help you
-double circumferenceOfPolygon(vector<Point> points)
-{
-    // WRITE SOLUTION HERE
-
-    //Change this
-    return 0.0;
+double circumferenceOfPolygon(vector<Point> points) {
+    double circumference = 0.0;
+    for (size_t i = 0; i < points.size(); ++i) {
+        // Calculate distance between consecutive points and add to circumference
+        circumference += distanceBetweenPoints(points[i], points[(i + 1) % points.size()]);
+    }
+    return circumference;
 }
 
-//You need to complete this - it reads the file with name strFile, and returns a vector listing all the points
-// 
-// See the spec to undertsand what these points represent
-vector<Point> readDataPoints(string strFile)
-{
-    //The vector where the final result will be stored
+vector<Point> readDataPoints(string strFile) {
+    // The vector where the final result will be stored
     vector<Point> points;
 
-    //WRITE SOLUTION HERE
- 
-    return points;;
+    // Open the file
+    ifstream inputFile(strFile);
+
+    // Check if the file is opened successfully
+    if (!inputFile.is_open()) {
+        cerr << "Error: Unable to open file " << strFile << endl;
+        return points; // Return an empty vector in case of an error
+    }
+
+    // Temporary variables to store x and y coordinates
+    double x, y;
+
+    // Ignore characters until '{' is encountered
+    char ch;
+    while (inputFile >> ch && ch != '{');
+
+    // Read points from the file until '}' is encountered
+    while (inputFile >> x >> y) {
+        // Create a Point and add it to the vector
+        points.push_back({x, y});
+    }
+
+    // Close the file
+    inputFile.close();
+
+    return points;
 }
