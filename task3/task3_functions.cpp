303767155ec467ad4575562b7e04252a3ec50b02
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-12-19 10:16:45 +0300

    task3 solution1

--- task3/task3_functions.cpp
+++ task3/task3_functions.cpp
@@ -2,15 +2,56 @@
 
 using namespace std;
 
-
 string findCorners(std::string filePath)
 {
-    string result;
+    // Open the file
+    ifstream file(filePath);
+    if (!file.is_open()) {
+        throw runtime_error("Error opening file");
+    }
 
-    // ************************
-    // WRITE YOUR SOLUTION HERE
-    // ************************
+    // Read the file line by line
+    string line;
+    int row = 0;
+    bool found = false;
+    int topLeftRow = -1, topLeftCol = -1, bottomRightRow = -1, bottomRightCol = -1;
 
- 
-    return result;
+    while (getline(file, line) && !found) {
+        istringstream iss(line);
+        int col = 0;
+        char ch;
+
+        // Scan across the columns
+        while (iss >> ch) {
+            if (ch == '*') {
+                // Found the top left corner
+                if (topLeftRow == -1) {
+                    topLeftRow = row;
+                    topLeftCol = col;
+                }
+                // Update the bottom right corner
+                bottomRightRow = row;
+                bottomRightCol = col;
+                found = true;
+            }
+            col++;
+        }
+
+        row++;
+    }
+
+    // Close the file
+    file.close();
+
+    // Check if a rectangle was found
+    if (topLeftRow == -1 || bottomRightRow == -1) {
+        return "{}";
+    }
+
+    // Build the result string
+    stringstream result;
+    result << "{ \"topLeft\" : [" << topLeftRow << "," << topLeftCol << "], \"bottomRight\" : ["
+           << bottomRightRow << "," << bottomRightCol << "] }";
+
+    return result.str();
 }
